b8d4368fe2eda9b91f068d6a89a91effb76c9328
Vinicius Pereira da Silva <viniciussilva32171@gmail.com>
2020-06-06 01:07:51 -0300

    Initial commit

---
+++ src/main/java/it/sup/mcepacientes/McePacientesApplication.java
@@ -0,0 +1,13 @@
+package it.sup.mcepacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McePacientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McePacientesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/sup/mcepacientes/McePacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sup.mcepacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McePacientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
